3b655730cd661c5a0648a63816059657ba408f52
michaelb0109 <mbernard3@zagmail.gonzaga.edu>
2022-01-20 14:29:35 -0800

    Updated Notes

--- main.py
+++ main.py
@@ -95,5 +95,16 @@ if x == 5:
   # this is the body
   #boolean evaluates to true
   # standard indentation is 1 tab = 4 spaces
-else:
+else: # if x not equal to 5
   print ("NOT TRUE")
+
+# we also have elif (else if) for multiple alternative if statements
+# use elif with multiple if conditions to test in a row-- the first one that is true will execute
+
+x = 5
+if x < 0:
+  print("x is negative")
+elif x > 0: #x > 0
+  print("x is positive")
+else:
+  print("x is 0")
